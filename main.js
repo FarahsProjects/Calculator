21839ea6521b49c7b85a127553ef4ec9f8b3cd0c
abeer-rawash <abeer.rawa96@gmail.com>
2020-10-04 11:45:14 +0300

    test

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-//hello
-//hii
+// //Adding button’s value when the screen is pressed.
+// Implementing backspace for input.
+// Clear expression screen (AC)
+$(document).ready(function(){
+ 
+
+})
+
+
+$(document).ready(function() { 
+            //Adding to the expression 
+            $(".design").click(function() { 
+                if (!$(this).hasClass("not")) { 
+                    if ($("#expression").val() == 0) 
+                        $("#expression").val($(this).text()); 
+                    else 
+                        $("#expression").val($( 
+                          "#expression").val() + $(this).text()); 
+                } 
+            }); 
